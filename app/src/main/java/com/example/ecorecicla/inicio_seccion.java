8067f660d59d17fc4233c420a423d62c5747d434
IsmaelMatiz <ismaelmatiz@gmail.com>
2024-07-22 00:52:56 -0500

    mejorada vista statistics y tips, conectadas las vistas excepto las de Leo, no las hizo

--- app/src/main/java/com/example/ecorecicla/inicio_seccion.java
+++ app/src/main/java/com/example/ecorecicla/inicio_seccion.java
@@ -1,6 +1,9 @@
 package com.example.ecorecicla;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,41 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class inicio_seccion extends AppCompatActivity {
 
+    Button btnGoBack;
+    Button btnStatistics;
+    Button btnTips;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_inicio_seccion);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnGoBack = findViewById(R.id.buttonReturnMain);
+        btnStatistics = findViewById(R.id.buttonStatisticsMain);
+        btnTips = findViewById(R.id.buttonTipsMain);
+
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToLogin = new Intent(inicio_seccion.this, MainActivity.class);
+                startActivity(goToLogin);
+            }
+        });
+
+        btnStatistics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToStatistics = new Intent(inicio_seccion.this, statistics.class);
+                startActivity(goToStatistics);
+            }
+        });
+
+        btnTips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToTips = new Intent(inicio_seccion.this, tips.class);
+                startActivity(goToTips);
+            }
         });
     }
 }
--- app/src/main/java/com/example/ecorecicla/tips.java
+++ app/src/main/java/com/example/ecorecicla/tips.java
@@ -1,6 +1,9 @@
 package com.example.ecorecicla;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class tips extends AppCompatActivity {
 
+    Button btnGoBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tips);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnGoBack = findViewById(R.id.buttonGoBackTips);
+
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToMain = new Intent(tips.this, inicio_seccion.class);
+                startActivity(goToMain);
+            }
         });
     }
 }
